9ac8dbfce6402f71bc7cb955d91021d77f0eab5d
= <=>
2023-08-06 11:42:10 -0400

    completed lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,37 @@
 from models import Dog
 
-def create_table(base):
-    pass
+
+def create_table(base, engine):
+    base.metadata.create_all(engine)
+
 
 def save(session, dog):
-    pass
+    session.add(dog)
+    session.commit()
+
 
 def get_all(session):
-    pass
+    query = session.query(Dog).all()
+    return query
+
 
 def find_by_name(session, name):
-    pass
+    query = session.query(Dog.name).filter(Dog.name.like(f"%{name}%")).all()[0]
+    return query
+
 
 def find_by_id(session, id):
-    pass
+    query = session.query(Dog).filter(Dog.id == id).all()[0]
+    return query
+
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    query = (
+        session.query(Dog).filter((Dog.name == name) and (Dog.breed == breed)).all()[0]
+    )
+    return query
+
 
 def update_breed(session, dog, breed):
-    pass
+    dog.breed = breed
+    session.commit()
